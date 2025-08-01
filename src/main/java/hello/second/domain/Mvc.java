ac89c86b574d833d0c60447cc48eb532be60d66f
leeyoojin <tometora44@gmail.com>
2025-08-01 15:11:13 +0900

    H2 서버와 연결

--- src/main/java/hello/second/domain/Mvc.java
+++ src/main/java/hello/second/domain/Mvc.java
@@ -1,7 +1,18 @@
 package hello.second.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Mvc {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
     private String name;
+    private String age;
 
     public String getName() {
         return name;
@@ -9,8 +20,6 @@ public class Mvc {
     public void setName(String name) {
         this.name = name;
     }
-
-    private String age;
 
     public String getAge() {
         return age;
